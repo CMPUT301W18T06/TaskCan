057b38508e3c0605e6a074ea3f2c8dd2f45ab3c5
ahuangJM <35260940+ahuangJM@users.noreply.github.com>
2018-03-08 11:30:30 -0700

    Implemented FileIO
    although this is feasible, but please read comments
    
    split this class into 2, one for input, one for output this class should have 2 static classes that the input and output class can inherent and implement and there are OpenFile and CloseFile!!!!

--- app/src/main/java/com/example/n8tech/taskcan/FileIO.java
+++ app/src/main/java/com/example/n8tech/taskcan/FileIO.java
@@ -16,9 +16,67 @@
 
 package com.example.n8tech.taskcan;
 
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
- * Created by cbinns on 2/22/2018.
+ * Created by ahuangJM on 2/22/2018.
+ *
+ * NOTE IMPORTANT *****
+ *
+ * split this class into 2, one for input, one for output
+ * this class should have 2 static classes that the input and output class
+ * can inherent and implement and there are OpenFile and CloseFile!!!!
  */
 
 public class FileIO {
+    private File file;
+    private BufferedWriter bufferedWriter;
+    private BufferedReader bufferedReader;
+    public FileIO(Context c) {
+        final String fileName = "home.cache";
+        this.file = new File(c.getCacheDir(), fileName);
+    }
+    public void OpenFile() {
+        try {
+            FileWriter fileWriter = new FileWriter(this.file.getAbsoluteFile());
+            FileReader fileReader = new FileReader(this.file.getAbsoluteFile());
+            this.bufferedWriter = new BufferedWriter(fileWriter);
+            this.bufferedReader = new BufferedReader(fileReader);
+        } catch (IOException e) {
+            Log.e("FileIO:", "Open File Failed", e);
+        }
+    }
+    public void WriteFile(String s) {
+        try {
+            this.bufferedWriter.write(s);
+            this.bufferedWriter.flush();
+        } catch (IOException e) {
+            Log.e("FileIO:", "Write File Failed", e);
+        }
+    }
+    public String ReadFile() {
+        char[] content = new char[100]; // is 100 enough or too much?
+        try {
+            this.bufferedReader.read(content);
+        } catch (IOException e) {
+            Log.e("FileIO:", "Read File Failed", e);
+        }
+        return String.valueOf(content);
+    }
+    public void CloseFile() {
+        try {
+            this.bufferedWriter.close();
+            this.bufferedReader.close();
+        } catch (IOException e) {
+            Log.e("FileIO:", "Close File Failed", e);
+        }
+    }
 }
