e00e01d2803175564e387141ee7d967db349a053
alexfilbert <afilbert@ualberta.ca>
2018-03-07 20:03:06 -0700

    Started Task Activity UI
    Probably need to look at implementing Fragments for TabHost to work properly.

--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -77,22 +77,22 @@ public final class R {
     public static final class id {
         public static final int adjust_height = 0x7f08001c;
         public static final int adjust_width = 0x7f08001d;
-        public static final int auto = 0x7f080022;
-        public static final int center = 0x7f080029;
-        public static final int dark = 0x7f080038;
-        public static final int hybrid = 0x7f080055;
-        public static final int icon_only = 0x7f080058;
-        public static final int light = 0x7f080060;
-        public static final int none = 0x7f08007c;
-        public static final int normal = 0x7f08007d;
-        public static final int radio = 0x7f08008b;
-        public static final int satellite = 0x7f080090;
-        public static final int standard = 0x7f0800bb;
-        public static final int terrain = 0x7f0800c1;
-        public static final int text = 0x7f0800c2;
-        public static final int text2 = 0x7f0800c3;
-        public static final int wide = 0x7f0800dc;
-        public static final int wrap_content = 0x7f0800df;
+        public static final int auto = 0x7f080024;
+        public static final int center = 0x7f08002b;
+        public static final int dark = 0x7f08003a;
+        public static final int hybrid = 0x7f080057;
+        public static final int icon_only = 0x7f08005a;
+        public static final int light = 0x7f080062;
+        public static final int none = 0x7f08007f;
+        public static final int normal = 0x7f080080;
+        public static final int radio = 0x7f08008e;
+        public static final int satellite = 0x7f080094;
+        public static final int standard = 0x7f0800bf;
+        public static final int terrain = 0x7f0800c5;
+        public static final int text = 0x7f0800c6;
+        public static final int text2 = 0x7f0800c7;
+        public static final int wide = 0x7f0800e0;
+        public static final int wrap_content = 0x7f0800e3;
     }
     public static final class integer {
         public static final int google_play_services_version = 0x7f090007;
--- app/src/main/java/com/example/n8tech/taskcan/MyTaskActivity.java
+++ app/src/main/java/com/example/n8tech/taskcan/MyTaskActivity.java
@@ -16,14 +16,131 @@
 
 package com.example.n8tech.taskcan;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TabHost;
 
 public class MyTaskActivity extends AppCompatActivity {
+
+    public static final String SEARCH_MESSAGE = "com.example.n8tech.taskcan.SEARCH_MESSAGE";
+    private EditText searchField;
+    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_task);
+        Toolbar mainToolbar = findViewById(R.id.menu_toolbar);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        setSupportActionBar(mainToolbar);
+        getSupportActionBar().setTitle("My Tasks");
+        mainToolbar.setTitle(Html.fromHtml("<font color='#FFFFFFF'>My Tasks </font>"));
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_24dp);
+
+        /*  TabHost appears to require a Fragment layout and this needs to go into onCreateView
+        TabHost tabHost = findViewById(android.R.id.tabhost);
+        tabHost.setup();
+
+        TabHost.TabSpec requestsTab = tabHost.newTabSpec("Requests Tab");
+        TabHost.TabSpec assignedTab = tabHost.newTabSpec("Assigned Tab");
+        TabHost.TabSpec archivedTab = tabHost.newTabSpec("Archived Tab");
+
+        requestsTab.setIndicator("Tab1");
+        //requestsTab.setContent(new Intent(this,TabActivity1.class));
+
+        assignedTab.setIndicator("Tab2");
+        //assignedTab.setContent(new Intent(this,TabActivity2.class));
+
+        archivedTab.setIndicator("Tab3");
+        //archivedTab.setContent(new Intent(this,TabActivity3.class));
+
+        tabHost.addTab(requestsTab);
+        tabHost.addTab(assignedTab);
+        tabHost.addTab(archivedTab); */
+
+        Intent intent = getIntent();
+        String userMsg = intent.getStringExtra(SignInActivity.USER_MESSAGE);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.nav_menu_home: {
+                                Intent goToHome = new Intent(MyTaskActivity.this,SearchActivity.class);
+                                startActivityForResult(goToHome, 1);
+
+                                break;
+                            }
+                            case R.id.nav_menu_my_tasks: {
+                                //Intent goToTasks = new Intent(MyTaskActivity.this, MyTaskActivity.class);
+                                //startActivityForResult(goToTasks, 1);
+
+                                break;
+                            }
+                            case R.id.nav_menu_my_bids: {
+                                Intent goToBids = new Intent(MyTaskActivity.this, MyBidActivity.class);
+                                startActivityForResult(goToBids, 1);
+
+                                break;
+                            }
+                            case R.id.nav_menu_my_profile: {
+                                Intent goToProfile = new Intent(MyTaskActivity.this, ViewProfileActivity.class);
+                                startActivityForResult(goToProfile, 1);
+
+                                break;
+                            }
+                            case R.id.nav_menu_sign_out: {
+                                Intent goToSignIn = new Intent(MyTaskActivity.this, SignInActivity.class);
+                                startActivityForResult(goToSignIn, 1);
+
+                                // Need to add sign out functionality
+
+                                break;
+                            }
+                        }
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+                        return false;
+                    }
+                });
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+        startActivity(intent);
+        //mainToolbar.setTitle(Html.fromHtml("<font color='#FFFFFFF'>Home </font>"));
+    }
+
 }
