320776fd2bc86ac9fb265f6a7f879e6974a9b2aa
alexfilbert <afilbert@ualberta.ca>
2018-03-15 15:14:22 -0600

    Added ImageView to TaskDetails XML

--- app/src/main/java/com/example/n8tech/taskcan/Views/TaskDetailActivity.java
+++ app/src/main/java/com/example/n8tech/taskcan/Views/TaskDetailActivity.java
@@ -16,16 +16,36 @@
 
 package com.example.n8tech.taskcan.Views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.n8tech.taskcan.R;
 
-public class TaskDetailActivity extends AppCompatActivity {
+public class TaskDetailActivity extends ActivityHeader {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_task_detail);
+
+    }
+
+    @Override
+    protected <T> void navigationView_itemOnClick(Class<T> nextClass) {
+        if (!this.getClass().equals(nextClass)) {
+            Intent i = new Intent(TaskDetailActivity.this, nextClass);
+            i.putExtra(SignInActivity.USER_MESSAGE, super.currentUser);
+            startActivity(i);
+        }
+    }
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_task_detail;
+    }
+
+    @Override
+    protected String getActivityTitle() {
+        return "Task Details";
     }
 }
