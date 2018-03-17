fc7b7cace28519e9cc7040f5e3506512806feadc
ahuangJM <35260940+ahuangJM@users.noreply.github.com>
2018-03-17 13:40:11 -0600

    slideshow complete

--- app/src/main/java/com/example/n8tech/taskcan/Views/ImageSlideShowActivity.java
+++ app/src/main/java/com/example/n8tech/taskcan/Views/ImageSlideShowActivity.java
@@ -17,17 +17,49 @@
 package com.example.n8tech.taskcan.Views;
 
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.n8tech.taskcan.Controller.SlideShowAdapter;
 import com.example.n8tech.taskcan.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import me.relex.circleindicator.CircleIndicator;
+
 public class ImageSlideShowActivity extends AppCompatActivity {
+    private static final String IMAGES_KEY = "ImageSlideShowActivity_IMAGES";
     private int[] images;
+    private ArrayList<Integer> slides;
+    private ViewPager mPager;
+    private int currentPage = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_slide_show);
-        this.images = getIntent().getIntArrayExtra("IMAGES");
+        this.images = getIntent().getIntArrayExtra(IMAGES_KEY);
+        this.initialialSlideShow();
+    }
+
+    private void initialialSlideShow() {
+        for (int i = 0; i < images.length; i++) {
+            this.slides.add(images[i]);
+        }
+        mPager = findViewById(R.id.pager);
+        mPager.setAdapter(new SlideShowAdapter(ImageSlideShowActivity.this, slides));
+        CircleIndicator indicator = findViewById(R.id.indicator);
+        indicator.setViewPager(mPager);
+
+        new Runnable() {
+            public void run() {
+                if (currentPage == images.length) {
+                    currentPage = 0;
+                }
+                mPager.setCurrentItem(currentPage++, true);
+            }
+        };
     }
 }
