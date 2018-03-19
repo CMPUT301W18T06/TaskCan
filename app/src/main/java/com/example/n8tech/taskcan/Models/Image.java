aea3d2ebc4d22d2ac87e4083083a1deda10765e5
ahuangJM <35260940+ahuangJM@users.noreply.github.com>
2018-03-19 10:12:54 -0600

    added image related classes and xmls

---
+++ app/src/main/java/com/example/n8tech/taskcan/Models/Image.java
@@ -0,0 +1,32 @@
+package com.example.n8tech.taskcan.Models;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by ahuang on 2018-03-19.
+ */
+
+public class Image {
+    private ArrayList<Integer> image;
+    public Image() {
+        this.image = new ArrayList<>();
+    }
+    public void setImagePixel(int position, int value) {
+        this.image.set(position, value);
+    }
+    public void setImage(ArrayList<Integer> image) {
+        this.image = image;
+    }
+    public ArrayList<Integer> getImageArrayList() {
+        return this.image;
+    }
+    public int[] getImageArray() {
+        int[] image = new int[this.image.size()];
+        for (int i=0; i < image.length; i++) {
+            image[i] = this.image.get(i);
+        }
+        return image;
+    }
+
+}
---
+++ app/src/main/java/com/example/n8tech/taskcan/Models/ImageList.java
@@ -0,0 +1,53 @@
+package com.example.n8tech.taskcan.Models;
+
+import android.support.annotation.NonNull;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+/**
+ * Created by ahuang on 2018-03-19.
+ */
+
+public class ImageList implements Iterable<Image> {
+    private ArrayList<Image> images;
+    public ImageList() {
+        this.images = new ArrayList<>();
+    }
+
+    public void addImage(Image image) {
+        images.add(image);
+    }
+    public void replace(int position, Image image) {
+        this.images.set(position, image);
+    }
+    public Image getImage(int position) {
+        return this.images.get(position);
+    }
+
+    @NonNull
+    @Override
+    public Iterator<Image> iterator() {
+        return new ImagesIterator();
+    }
+
+    private class ImagesIterator implements Iterator<Image> {
+
+        private int index = 0;
+
+        public boolean hasNext() {
+            return (this.index < images.size());
+        }
+
+        public Image next() {
+            return images.get(index++);
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException("not supported yet");
+        }
+    }
+}
