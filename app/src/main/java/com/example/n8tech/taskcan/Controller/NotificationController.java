d9d444e8529883c4b1761cedcb11c22772450b67
ahuangJM <35260940+ahuangJM@users.noreply.github.com>
2018-03-21 17:55:20 -0600

    create channel implemented

--- app/src/main/java/com/example/n8tech/taskcan/Controller/NotificationController.java
+++ app/src/main/java/com/example/n8tech/taskcan/Controller/NotificationController.java
@@ -1,6 +1,11 @@
 package com.example.n8tech.taskcan.Controller;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import com.example.n8tech.taskcan.Models.NotificationContent;
 import com.example.n8tech.taskcan.R;
@@ -10,16 +15,46 @@ import com.example.n8tech.taskcan.R;
  */
 
 public class NotificationController {
+    public static final String ANDROID_CHANNEL_ID = "com.example.n8tech.taskcan";
+    public static final String ANDROID_CHANNEL_NAME = "TASKCAN NOTIFICATION CHANNEL";
+
     private NotificationContent nContent;
     private NotificationCompat.Builder nBuilder;
+    private NotificationManager nManager;
 
     public NotificationController(NotificationContent notification) {
         this.nContent = notification;
+        this.createManager();
+        this.createChannel();
+        this.createBuilder();
+    }
+
+    private void createManager() {
+        this.nManager = (NotificationManager) this.nContent.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    public void createBuilder() {
         this.nBuilder = new NotificationCompat.Builder(this.nContent.getContext(), this.nContent.getChannelID())
                 .setSmallIcon(R.drawable.alert)
                 .setContentTitle(this.nContent.getTitle())
                 .setContentText(this.nContent.getDescription())
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+    }
+
+    public void createChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel androidChannel = new NotificationChannel(ANDROID_CHANNEL_ID,
+                    ANDROID_CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            androidChannel.enableLights(true);
+            androidChannel.enableVibration(true);
+            androidChannel.setLightColor(Color.GREEN);
+            androidChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            getManager().createNotificationChannel(androidChannel);
+        }
+    }
+
+    private NotificationManager getManager() {
+        return this.nManager;
     }
 
     public NotificationCompat.Builder getBuilder() {
