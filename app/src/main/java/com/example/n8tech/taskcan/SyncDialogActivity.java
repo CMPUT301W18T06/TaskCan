4996a0c08270a69643d083936279213dbfe8657f
ahuangJM <35260940+ahuangJM@users.noreply.github.com>
2018-03-23 09:53:07 -0600

    progress dialog layout activity created

--- app/src/main/java/com/example/n8tech/taskcan/SyncDialogActivity.java
+++ app/src/main/java/com/example/n8tech/taskcan/SyncDialogActivity.java
@@ -1,7 +1,10 @@
 package com.example.n8tech.taskcan;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SyncDialogActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class SyncDialogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sync_dialog);
         this.setFinishOnTouchOutside(false);
+
+        this.resetProgressBarWidth();
+    }
+
+    private void resetProgressBarWidth() {
+        ProgressBar pb = findViewById(R.id.progressBar);
+        TextView tv = findViewById(R.id.syncMessage);
+        pb.setLayoutParams(new ConstraintLayout.LayoutParams(tv.getWidth(), ConstraintLayout.LayoutParams.WRAP_CONTENT));
     }
 }
