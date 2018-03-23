ca6a1323ee7cd287ede5bbb285c94903b9b1fe6f
ahuangJM <35260940+ahuangJM@users.noreply.github.com>
2018-03-23 10:45:39 -0600

    sync dialog activity done for now... require attention of Q

--- app/src/main/java/com/example/n8tech/taskcan/Services/SyncService.java
+++ app/src/main/java/com/example/n8tech/taskcan/Services/SyncService.java
@@ -1,16 +1,30 @@
 package com.example.n8tech.taskcan.Services;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.Nullable;
+
+import com.example.n8tech.taskcan.SyncDialogActivity;
 
 /**
  * Created by AlanJ on 2018-03-23.
  */
 
 public class SyncService extends IntentService {
+    public static final String SYNC_SERVICE_NUM_OF_SYNC = "SYNC_SERVICE_NUM_OF_SYNC";
+
+    private boolean connectionStatus;
+    private ConnectivityManager connManager;
+    private NetworkInfo mWifi;
     public SyncService() {
         super("SyncService");
+        this.connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        this.mWifi = this.connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        this.connectionStatus = this.mWifi.isConnected();
     }
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
@@ -23,6 +37,21 @@ public class SyncService extends IntentService {
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
-        
+        while (true) {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (this.mWifi.isConnected() && !this.connectionStatus) {
+                this.connectionStatus = true;
+                // https://stackoverflow.com/questions/3606596/android-start-activity-from-service
+                Intent dialogIntent = new Intent(this, SyncDialogActivity.class);
+                dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(dialogIntent);
+            } else {
+                this.connectionStatus = false;
+            }
+        }
     }
 }
--- app/src/main/java/com/example/n8tech/taskcan/SyncDialogActivity.java
+++ app/src/main/java/com/example/n8tech/taskcan/SyncDialogActivity.java
@@ -1,25 +1,85 @@
 package com.example.n8tech.taskcan;
 
+import android.os.Handler;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.n8tech.taskcan.Services.SyncService;
+
+// https://www.mkyong.com/android/android-progress-bar-example/
 public class SyncDialogActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    private Handler handler;
+    private int progressBarStatus;
+    private int incrementer;
+    private int numOfSyncs;
+    private final int MAX_PROGRESS = 100;
+    private TextView syncMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sync_dialog);
         this.setFinishOnTouchOutside(false);
-
+        this.progressBar = findViewById(R.id.progressBar);
+        this.loadExtras();
         this.resetProgressBarWidth();
+        this.progressAction();
+    }
+
+    public void loadExtras() {
+        this.numOfSyncs = getIntent().getExtras().getInt(SyncService.SYNC_SERVICE_NUM_OF_SYNC);
+        this.incrementer = MAX_PROGRESS / this.numOfSyncs;
+    }
+
+    private void progressAction() {
+        this.progressBarStatus = 0;
+        this.progressBar.setProgress(progressBarStatus);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (progressBarStatus < 100) {
+                    // update one item
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                this.updateProgressBar();
+                if (progressBarStatus >= 100) {
+                    syncMessage.setText("Sync Compleeted ...");
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    finishActivity(-1);
+                }
+            }
+
+            private void updateProgressBar() {
+                progressBarStatus += incrementer;
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressBar.setProgress(progressBarStatus);
+                    }
+                });
+            }
+        });
     }
 
     private void resetProgressBarWidth() {
-        ProgressBar pb = findViewById(R.id.progressBar);
-        TextView tv = findViewById(R.id.syncMessage);
-        pb.setLayoutParams(new ConstraintLayout.LayoutParams(tv.getWidth(), ConstraintLayout.LayoutParams.WRAP_CONTENT));
+        this.syncMessage = findViewById(R.id.syncMessage);
+        this.progressBar.setLayoutParams(new ConstraintLayout.LayoutParams(this.syncMessage.getWidth(), ConstraintLayout.LayoutParams.WRAP_CONTENT));
     }
+
+    @Override
+    public void onBackPressed() { }
+
 }
