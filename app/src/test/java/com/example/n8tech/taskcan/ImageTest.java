72ae2d32f1d878463532d29d169117e78f9a96c2
Jeanna Somoza <somoza@cs.ualberta.ca>
2018-04-07 14:32:47 -0600

    minor changes

--- app/src/test/java/com/example/n8tech/taskcan/ImageTest.java
+++ app/src/test/java/com/example/n8tech/taskcan/ImageTest.java
@@ -17,6 +17,8 @@ import static org.junit.Assert.*;
  */
 
 public class ImageTest {
+    private Bitmap bitmap;
+    private int[] image_array;
 
     public ImageTest(){
 
@@ -24,10 +26,11 @@ public class ImageTest {
 
     @Test
     public void testImageConstructors(){
-        int[] image_array = new int[100 * 100];
-        Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        image_array = new int[bitmap.getWidth() * bitmap.getHeight()];
+        bitmap.setPixels(image_array, 0, 100,0,0,100,100);
+        Arrays.fill(image_array, 100);
         Image image1 = new Image(bitmap);
-        bitmap.getPixels(image_array, 0, 100, 0,0, 100, 100);
         Image image2 = new Image(image_array, 100, 100);
         assertEquals(image1, image2);
     }
@@ -43,5 +46,7 @@ public class ImageTest {
         Image image2 = new Image(bitmap);
         assertEquals(image1, image2);
     }
+
+
 
 }
