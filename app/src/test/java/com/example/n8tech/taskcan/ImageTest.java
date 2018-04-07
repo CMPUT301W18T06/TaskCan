cacb1efd6f9dae12197dca48711f59fb5db003e7
Jeanna Somoza <somoza@cs.ualberta.ca>
2018-04-07 17:01:33 -0600

    image testing

--- app/src/test/java/com/example/n8tech/taskcan/ImageTest.java
+++ app/src/test/java/com/example/n8tech/taskcan/ImageTest.java
@@ -25,8 +25,9 @@ public class ImageTest {
     @Test
     public void testImageConstructors(){
 
-        int[] image_array = {1, 1, 1, 2,2,2,0,0,0};
+        int[] image_array = {1,1,1,2,2,2,0,0,0};
         Image image1 = new Image(image_array, 3, 3);
+        image1.recreateRecycledBitmap();
         Bitmap bitmap = Bitmap.createBitmap(3 ,3,  Bitmap.Config.ARGB_8888);
         bitmap.getPixels(image_array, 0, 3, 0, 0, 3, 3);
         Image image2 = new Image(bitmap);
