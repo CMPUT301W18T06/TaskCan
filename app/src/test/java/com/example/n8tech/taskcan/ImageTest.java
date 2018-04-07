c11603d0b6201b7f606871d90ea13680174847db
Jeanna Somoza <somoza@cs.ualberta.ca>
2018-04-07 12:51:08 -0600

    more unit testing

--- app/src/test/java/com/example/n8tech/taskcan/ImageTest.java
+++ app/src/test/java/com/example/n8tech/taskcan/ImageTest.java
@@ -4,6 +4,9 @@ import com.example.n8tech.taskcan.Models.Image;
 import android.graphics.Bitmap;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 /**
@@ -21,7 +24,6 @@ public class ImageTest {
 
     @Test
     public void testImageConstructors(){
-
         int[] image_array = new int[100 * 100];
         Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
         Image image1 = new Image(bitmap);
@@ -32,6 +34,14 @@ public class ImageTest {
 
     @Test
     public void testRecreateRecycledBitmap(){
-
+        int[] image_array = new int[100 * 100];
+        Arrays.fill(image_array, 50);
+        Image image1 = new Image(image_array, 100, 100);
+        image1.recreateRecycledBitmap();
+        Bitmap bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        bitmap.getPixels(image_array, 0, 100, 0,0, 100, 100);
+        Image image2 = new Image(bitmap);
+        assertEquals(image1, image2);
     }
+
 }
