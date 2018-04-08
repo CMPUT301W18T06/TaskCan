eadcb24f59678909c128ab563dff4d5e1ba874a7
Nathanael Belayneh <nbelayne@cs.ualberta.ca>
2018-04-07 18:19:37 -0600

    update Current Bid

--- app/src/main/java/com/example/n8tech/taskcan/Views/ViewTaskActivity.java
+++ app/src/main/java/com/example/n8tech/taskcan/Views/ViewTaskActivity.java
@@ -205,6 +205,7 @@ public class ViewTaskActivity extends ActivityHeader{
             if (bids.getBidUsername().intern() == currentUser.getUsername().intern()){
                 bidIndex = taskBidList.getBidIndex(bids);
                 task.replaceBidAtIndex(bidIndex, bid);
+                task.updateCurrentBid();
                 newBid = false;
                 break;
             }
